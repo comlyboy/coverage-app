c712afe17c5140bc29c420ab9b27916838805652
Cornelius Okeke <okekecornelius@gmail.com>
2024-12-19 18:06:24 +0100

    added  cdk build

--- backend/cdk/bin/coverageApiApp.ts
+++ backend/cdk/bin/coverageApiApp.ts
@@ -11,18 +11,21 @@ const app = new App();
 // get cdk command context.
 // it contains all the setup inside context cdk.json file and other flags passed running cdk command like synth, bootsrap or deploy.
 // Used it here to get stage and region information for deployment.
-const regions = app.node.tryGetContext('regions')?.split(',') as string[] || [];
-const stage = app.node.tryGetContext('stage') as StageType || null;
+const regions = app.node.tryGetContext('regions')?.split(',') as string[] || ['us-east-1'];
+const stage = app.node.tryGetContext('stage') as StageType || 'dev';
 
-if (!stage || !regions.length) {
-	throw new Error('invalid region or stage selected!')
-}
+// if (!stage || !regions.length) {
+// 	throw new Error('invalid region or stage selected!')
+// }
 
 regions.forEach(region => {
 	new CoverageApiStack(app, `${LambdaApplicationEnum.STACK_NAME}_${stage}_${region}`, {
 		stage,
 		stackName: `${LambdaApplicationEnum.STACK_NAME}-${stage}`,
-		env: { region },
+		env: {
+			region,
+			account: process.env.CDK_DEFAULT_ACCOUNT,
+		},
 		tags: {
 			appName: LambdaApplicationEnum.STACK_NAME
 		}
